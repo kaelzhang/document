389eb90b1aa954ad29fff4449702ebb8acaf367c
kaelzhang <i@kael.me>
2013-10-15 23:00:00 +0800

    complete; UNTESTED

--- test/modules/parser.js
+++ test/modules/parser.js
@@ -1,29 +1,18 @@
 'use strict';
 
-var parser = require('../../lib/parser');
-var config = require('../../lib/config');
+var dao = require('../../lib/dao');
 
 var expect = require('chai').expect;
-
 var node_path = require('path');
 
-describe("description", function(){
-    it("description", function(done){
-        var doc_root = node_path.resolve('test/repo/doc');
+var doc_root = node_path.resolve('test/repo/doc');
 
-        parser.raw_tree( doc_root, function (err, tree) {
-            done();
-            console.log('raw tree:', tree);
-
-            var cfg = config.get_config({
-                doc_root: doc_root
-            });
-
-            console.log('cfg', cfg)
-
-            var ltree = parser.language_tree(tree, cfg.languages);
-
-            console.log('language tree', ltree)
-        });
-    });
+dao.docs(doc_root, function (err, data) {
+    console.log(
+        require('util').inspect(data, {
+            depth: 10
+        })
+    );
 });
+
+        
