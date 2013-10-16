f4e66c573579b6313205044780476c3a967e0f6c
kaelzhang <i@kael.me>
2013-10-16 11:38:06 +0800

    update argv schema

---
+++ bin/schema.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// options and argv schema
+// only used by bin
+
+var node_path = require('path');
+var fs = require('fs-sync');
+
+exports.shorthands = {
+    c: 'cwd',
+    h: 'help',
+    p: 'port'
+};
+
+exports.schema = {
+    help: {
+        type: Boolean
+    },
+
+    cwd: {
+        type: node_path,
+        default: process.cwd()
+    },
+
+    doc: {
+        setter: function (doc, is_default) {
+            // defined, but not an absolute path
+            if ( doc && doc !== node_path.resolve(doc) ) {
+                doc = node_path.resolve(this.get('cwd'), value);
+            }
+
+            return doc;
+        }
+    },
+
+    port: {
+        type: Number,
+        // d0mT
+        default: 9037
+    },
+
+    theme: {
+        type: node_path,
+        default: node_path.join(__dirname, '..', 'theme')
+    }
+};
